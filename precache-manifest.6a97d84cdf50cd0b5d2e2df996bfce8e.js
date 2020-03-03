26247ff5623001a2fe6d03926ac43dbf963b3d29
FarzanaPrianka <farzana.prianka@gmail.com>
2020-03-03 14:56:18 -0500

    Updates

---
+++ precache-manifest.6a97d84cdf50cd0b5d2e2df996bfce8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded4b4a16e9929890d9e47823039e3fd",
+    "url": "/project-five/index.html"
+  },
+  {
+    "revision": "f68a001bbfcf2bdf20a0",
+    "url": "/project-five/static/css/main.3738ef90.chunk.css"
+  },
+  {
+    "revision": "875e906f041ebd78ceca",
+    "url": "/project-five/static/js/2.8286716a.chunk.js"
+  },
+  {
+    "revision": "07a707eba59e3e3ed782f77b31f34780",
+    "url": "/project-five/static/js/2.8286716a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f68a001bbfcf2bdf20a0",
+    "url": "/project-five/static/js/main.50ccd605.chunk.js"
+  },
+  {
+    "revision": "72acaf2498cddbce7bc0",
+    "url": "/project-five/static/js/runtime-main.f0e583b8.js"
+  }
+]);
